85a480aba4cf7eca499517060ece89b63a9b289a
Vladimir Ciobanu <admin@cvlad.info>
2018-02-08 13:54:15 +0200

    more examples

--- src/Main.purs
+++ src/Main.purs
@@ -7,20 +7,35 @@ import Control.Monad.Eff.Console (CONSOLE, log)
 import Data.Symbol (class IsSymbol, SProxy)
 import Data.Variant (Variant, inj)
 
-class IsSymbol sym <= Rel parent child sym | sym → child where
+-- | This compiles just fine.
+class WithoutSym t where
+  wrel ∷ ∀ sym r1 r2
+       . IsSymbol sym
+       ⇒ RowCons sym t r1 r2
+       ⇒ SProxy sym
+       → t
+       → Variant r2
+
+instance wsInt ∷ WithoutSym Int where
+  wrel sym p = inj sym p
+
+-- This does not work.
+class IsSymbol sym <= Rel t sym where
   rel ∷ ∀ r1 r2
-      . RowCons sym child r1 r2
+      . RowCons sym t r1 r2
       ⇒ SProxy sym
-      → parent
+      → t
       → Variant r2
 
-instance relInt ∷ Rel Int Int "int" where
+instance relInt ∷ Rel Int "test" where
+-- | Compile error is here, at inj.
   rel sym p = inj sym p
 
-rel' ∷ ∀ r1 r2 parent child sym
+-- | This works just fine as well.
+rel' ∷ ∀ r1 r2 t sym
      . IsSymbol sym
-     ⇒ RowCons sym child r1 r2
+     ⇒ RowCons sym t r1 r2
      ⇒ SProxy sym
-     → parent
+     → t
      → Variant r2
 rel' sym p = inj sym p
